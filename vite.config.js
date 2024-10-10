009fe7544665ea62f29ddd416c584c919c7c9cae
Gabriela Silva <67657538+gabisantosi@users.noreply.github.com>
2024-10-10 16:24:37 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,8 @@
-import { fileURLToPath, URL } from "url";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: "8080",
-  },
   plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-      {
-        find: "lib",
-        replacement: resolve(__dirname, "lib"),
-      },
-    ],
-  },
-});
+  base: '/healthwise-boost', // Substitua 'kure-saude-digital' pelo nome do seu repositório
+})
